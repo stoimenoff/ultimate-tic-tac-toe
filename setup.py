b4180195a158c19892eacac335c74edc72ffd2a2
stoimenoff <stoimenoffjr@gmail.com>
2016-05-24 19:14:53 +0300

    Setup + initial requirements

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ultimate-tic-tac-toe",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=['PyQt5>=5.6'],
+    author="Svetomir Stoimenov",
+    description="The Ultimate Tic-Tac-Toe game",
+    license="MIT",
+    keywords="ultimate tic-tac-toe",
+)
