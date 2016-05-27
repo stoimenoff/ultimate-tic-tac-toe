9122137d093e12886084d66d9015094a9ff93b82
stoimenoff <stoimenoffjr@gmail.com>
2016-05-27 22:23:24 +0300

    Game class modified

--- game/game.py
+++ game/game.py
@@ -7,10 +7,14 @@ class Game:
         self.players = [player1, player2]
         self.on_turn = 0
 
-    def start(self):
-        while self.macroboard.state == boards.State.IN_PROGRESS:
-            print(self.macroboard)
+    def move(self):
+        if self.macroboard.state == boards.State.IN_PROGRESS:
             move = self.players[self.on_turn].choose_move(self.macroboard)
             self.macroboard.make_move(*move)
             self.on_turn = abs(self.on_turn - 1)
+
+    def play(self):
+        while self.macroboard.state == boards.State.IN_PROGRESS:
+            print(self.macroboard)
+            self.move()
         print(self.macroboard)
