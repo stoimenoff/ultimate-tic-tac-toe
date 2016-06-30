0b69ed95f6975c01189d8ecb2b45b69876d0df44
stoimenoff <stoimenoffjr@gmail.com>
2016-06-30 17:57:56 +0300

    Single player widget updated

--- gui/singleplayergame.py
+++ gui/singleplayergame.py
@@ -1,6 +1,7 @@
 import game
 from . import QGame
-from PyQt5.QtWidgets import QWidget, QVBoxLayout, QPushButton, QLabel
+from PyQt5.QtWidgets import (QWidget, QGridLayout, QPushButton,
+                             QLabel, QLCDNumber)
 
 
 class SinglePlayerGame(QWidget):
@@ -12,16 +13,25 @@ class SinglePlayerGame(QWidget):
         self.opponentScore = 0
         self.playerIsNotFirst = False
 
-        layoyt = QVBoxLayout()
+        self.playerScoreLcd = QLCDNumber(2)
+        self.playerScoreLcd.setSegmentStyle(QLCDNumber.Filled)
+        self.opponentScoreLcd = QLCDNumber(2)
+        self.opponentScoreLcd.setSegmentStyle(QLCDNumber.Filled)
+
+        layoyt = QGridLayout()
         self.scoreLabel = QLabel('Score: ')
         # tmp
         self.gameWidget = QGame(game.players.ai.EuristicsBot('Bot'))
         self.gameWidget.gameEnded.connect(self.updateScoreAndReset)
         self.exitButton = QPushButton('Exit to menu')
-        layoyt.addWidget(self.scoreLabel)
-        layoyt.addWidget(self.gameWidget)
-        layoyt.addWidget(self.exitButton)
+        layoyt.addWidget(QLabel('You'), 0, 0)
+        layoyt.addWidget(QLabel('Opponent'), 0, 1)
+        layoyt.addWidget(self.playerScoreLcd, 1, 0)
+        layoyt.addWidget(self.opponentScoreLcd, 1, 1)
+        layoyt.addWidget(self.gameWidget, 2, 0, 1, 2)
+        layoyt.addWidget(self.exitButton, 3, 0)
         self.setLayout(layoyt)
+        self.resize(400, 750)
 
     def updateScoreAndReset(self):
         print('usr')
@@ -29,11 +39,14 @@ class SinglePlayerGame(QWidget):
         self.playerIsNotFirst = not self.playerIsNotFirst
         result = self.gameWidget.board.state
         if result == game.boards.State.X_WON:
-            self.playerScore += 2
+            self.playerScore += 3
         elif result == game.boards.State.O_WON:
-            self.opponentScore += 2
-        self.playerScore += 1
-        self.opponentScore += 1
+            self.opponentScore += 3
+        elif result == game.boards.State.DRAW:
+            self.playerScore += 1
+            self.opponentScore += 1
+        self.playerScoreLcd.display(self.playerScore)
+        self.opponentScoreLcd.display(self.opponentScore)
         if self.numberOfGames > 0:
             self.gameWidget.reset(self.playerIsNotFirst)
         else:
--- test.py
+++ test.py
@@ -1,12 +1,12 @@
 import sys
 from gui import *
-from gui.singleplayermenu import SinglePlayerGame
+from gui.singleplayergame import SinglePlayerGame
 from PyQt5.QtWidgets import QApplication
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     # window = QGame()
-    window = SinglePlayerGame(1, 1)
+    window = SinglePlayerGame(1, 2)
     window.show()
     sys.exit(app.exec_())
