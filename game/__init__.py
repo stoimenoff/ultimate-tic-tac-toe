91d94e6ae5de27e100eab1dcf239a59dddb91c55
stoimenoff <stoimenoffjr@gmail.com>
2016-07-03 16:15:35 +0300

    Deleted unnecessary files

--- game/__init__.py
+++ game/__init__.py
@@ -1,5 +1,4 @@
 from .game import Game
-from .servergame import ServerGame
 from . import boards
 from . import players
-__all__ = ['Game', 'ServerGame', 'boards', 'players']
+__all__ = ['Game', 'boards', 'players']
--- game/servergame.py
+++
@@ -1,2 +0,0 @@
-class ServerGame:
-    pass
