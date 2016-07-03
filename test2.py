c0638817124d657d48f6b4dc8f36243478cdad79
stoimenoff <stoimenoffjr@gmail.com>
2016-07-03 20:40:14 +0300

    .gitingore updated'

--- test2.py
+++ test2.py
@@ -1,22 +1,11 @@
-import sys
-from gui import *
-from gui.multiplayerclient import ClientGame
-from gui.botbattle import BotBattle
-from gui.spectatebotbattle import BotBattleMenu, SpectateBattle
+from ultimatetictactoe.gui.multiplayer import ClientGame
 from PyQt5.QtWidgets import QApplication
-import game
-# import pickle
 
 
 if __name__ == '__main__':
+    import sys
     app = QApplication(sys.argv)
-    # window = SinglePlayerGame(1, 1)
-    # window = SinglePlayer()
-    # window = HotSeatGame()
-    # window = ClientGame()
-    # window = BotBattle(game.players.ai.EuristicsBot('B1'),
-                       # game.players.ai.EuristicsBot('B2'))
-    window = SpectateBattle()
+    window = ClientGame()
     window.move(50, 50)
     window.show()
     sys.exit(app.exec_())
