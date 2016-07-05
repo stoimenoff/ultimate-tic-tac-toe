c237ca65ea1ef14bba936ffc9c2f47337426007b
stoimenoff <stoimenoffjr@gmail.com>
2016-07-05 22:57:54 +0300

    Boards docstrings added

--- ultimatetictactoe/game/boards/macroboard.py
+++ ultimatetictactoe/game/boards/macroboard.py
@@ -4,6 +4,27 @@ from copy import deepcopy
 
 
 class Macroboard:
+    """
+    Represents the big board of the game by a grid of Microboards.
+
+    By default the board is full of empty microboards.
+
+    Size of the board is passed to the constructor and can differ
+    between 3 and 9. Invalid size raises a ValueError.
+    Microboards are numbered by rows and columns, starting from 0.
+
+    Keeps track of the moves made and the player who is on turn.
+    By default the first player to play is Square.X.
+    Optional parameter in the constructor can change that.
+
+    *Note*
+    Squares in the microboards are numbered by rows and columns,
+    from 0 to size**2 The (row, column) of the square is its position.
+    A square's coordinates is a tuple of four elements (x, y, i, j),
+    where the (x, y) are the row and column of the microboard,
+    the square is in, and (i, j) are the row and column of the square in
+    that microboard.
+    """
     def __init__(self, x_is_first=True, size=3):
         if size < 3 or size > 9:
             raise ValueError('Invalid board size!')
@@ -16,36 +37,64 @@ class Macroboard:
 
     @property
     def dead_boards(self):
+        """
+        Returns a list of (row, column) of the microboards
+        that are not in progress.
+        """
         return [(i, j) for i in range(self.SIZE) for j in range(self.SIZE)
                 if self.boards[i][j].state != State.IN_PROGRESS]
 
     @property
     def active_boards(self):
+        """
+        Returns a list of (row, column) of the microboards
+        that are in progress.
+        """
         return [(i, j) for i in range(self.SIZE) for j in range(self.SIZE)
                 if self.boards[i][j].state == State.IN_PROGRESS]
 
     def to_coords(self, px, py):
+        """
+        Converts a square position (px, py) to square coordinates (x, y, i, j).
+        """
         if px not in range(self.SIZE**2) or py not in range(self.SIZE**2):
             raise IndexError
         return (px // self.SIZE, py // self.SIZE,
                 px % self.SIZE, py % self.SIZE)
 
     def to_position(self, x, y, i, j):
+        """
+        Converts a square coordinates (x, y, i, j) to square position (px, py).
+        """
         return (x * self.SIZE + i, y * self.SIZE + j)
 
     def coords_to_positions(self, coords):
+        """
+        Converts a list of coordinates to a list of positions.
+        """
         return [self.to_position(x, y, i, j) for (x, y, i, j) in coords]
 
     def positions_to_coords(self, positions):
+        """
+        Converts a list of positions to a list of coordinates.
+        """
         return [self.to_coords(px, py) for (px, py) in positions]
 
     def board_empty_positions(self, x, y):
+        """
+        Returns the positions of the empty squares in the (x, y) microboard.
+        """
         board = self.boards[x][y]
         coords = [(x, y, i, j) for (i, j) in board.empty_squares]
         return self.coords_to_positions(coords)
 
     @property
     def available_boards(self):
+        """
+        A list of (row, column) of the boards that can currently be played.
+        *Note* Depends on the last move that was made.
+        Returns and empty list if the game has ended.
+        """
         if self.state != State.IN_PROGRESS:
             return []
         if self.last_move is None:
@@ -56,6 +105,11 @@ class Macroboard:
         return self.active_boards
 
     def can_play_on_all_active(self):
+        """
+        Returns True if a move is currently possible on all of the
+        active microboards.
+        Returns False otherwise.
+        """
         if self.last_move is None:
             return True
         x, y = self.last_move[-2:]
@@ -65,6 +119,9 @@ class Macroboard:
 
     @property
     def available_moves(self):
+        """
+        A list of the positions of all possible moves.
+        """
         moves = []
         for x, y in self.available_boards:
             moves.extend([self.to_position(x, y, i, j) for (i, j)
@@ -73,6 +130,10 @@ class Macroboard:
 
     @property
     def state(self):
+        """
+        The current state of the game.
+        One of State.X_WON, State.O_WON, State.DRAW, State.IN_PROGRESS
+        """
         lines = self.state_lines()
         for line in lines:
             if set(line) == {State.X_WON}:
@@ -85,9 +146,20 @@ class Macroboard:
 
     @property
     def has_a_winner(self):
+        """
+        True, if a player has won the game,
+        False otherwise.
+        """
         return self.state in {State.X_WON, State.O_WON}
 
     def make_move(self, px, py):
+        """
+        Makes a move on the position (px, py).
+        The player who is on turn is determined automatically.
+
+        If the game has ended raises GameEndedError.
+        If the move is not available raises IllegalMoveError.
+        """
         if self.state != State.IN_PROGRESS:
             raise GameEndedError('Cannot make move. The game has ended.')
         x, y, i, j = self.to_coords(px, py)
@@ -100,6 +172,9 @@ class Macroboard:
         self.history.append(self.last_move)
 
     def undo_last_move(self):
+        """
+        Undo the last move made.
+        """
         if self.last_move is None:
             return
         x, y, i, j = self.last_move
@@ -112,6 +187,10 @@ class Macroboard:
         del self.history[-1]
 
     def get_on_turn(self):
+        """
+        Return the player who is on turn.
+        Either Square.X or Square.O
+        """
         return deepcopy(self.__on_turn)
 
     def state_lines(self):
@@ -124,6 +203,10 @@ class Macroboard:
         return lines
 
     def winner(self):
+        """
+        Returns Square.X or Square.O if the corresponding player won the game.
+        returns None otherwise.
+        """
         state = self.state
         if state == State.X_WON:
             return Square.X
@@ -132,6 +215,10 @@ class Macroboard:
         return None
 
     def __str__(self):
+        """
+        String representation of the macroboard for
+        visualizing in the command line.
+        """
         str = '-' * (self.SIZE ** 2 + self.SIZE + 1) + '\n'
         for row in self.boards:
             for i in range(self.SIZE):
@@ -145,6 +232,10 @@ class Macroboard:
         return str
 
     def macro_str(self):
+        """
+        String representation of the microboards' results for
+        visualizing in the command line.
+        """
         str = '-' * (2 * self.SIZE + 1) + '\n'
         for row in self.boards:
             str += ' '
--- ultimatetictactoe/game/boards/microboard.py
+++ ultimatetictactoe/game/boards/microboard.py
@@ -3,6 +3,18 @@ from .boarditems import State, Square, IllegalMoveError
 
 
 class Microboard:
+    """
+    Represents one of the small boards in the game.
+
+    By default the board is full of empty squares.
+
+    Size of the board is passed to the constructor and can differ
+    between 3 and 9. Invalid size raises a ValueError.
+    Board squares are numbered by rows and columns, starting from 0.
+    Square value is changed via 'set_square'.
+
+    Keeps track of the moves made.
+    """
     def __init__(self, size=3):
         if size < 3 or size > 9:
             raise ValueError('Invalid board size!')
@@ -13,6 +25,10 @@ class Microboard:
 
     @property
     def state(self):
+        """
+        The current state of the board.
+        One of State.X_WON, State.O_WON, State.DRAW, State.IN_PROGRESS
+        """
         lines = self.lines()
         for line in lines:
             if set(line) == {Square.X}:
@@ -24,6 +40,15 @@ class Microboard:
         return State.IN_PROGRESS
 
     def set_square(self, x, y, value):
+        """
+        x and y represent the square's row and column.
+
+        value should be one of Square.X or Square.O
+        ValueError is raised otherwise
+
+        If the board is not in play or the square at (x, y)
+        is not Empty IllegalMoveError is raised.
+        """
         if self.state != State.IN_PROGRESS:
             raise IllegalMoveError('Board is dead!')
         if self.grid[x][y] != Square.EMPTY:
@@ -35,6 +60,9 @@ class Microboard:
         self.history.append(self.last_move)
 
     def undo_last_move(self):
+        """
+        Undo the last move made on the board.
+        """
         if self.last_move is None:
             return
         x, y = self.last_move
@@ -47,13 +75,23 @@ class Microboard:
 
     @property
     def empty_squares(self):
+        """
+        A list of tuples (row, column) of the squares that are currently empty.
+        """
         return [(i, j) for i in range(self.SIZE) for j in range(self.SIZE)
                 if self.grid[i][j] == Square.EMPTY]
 
     def export_grid(self):
+        """
+        Deepcopy of the board's grid.
+        """
         return deepcopy(self.grid)
 
     def winner(self):
+        """
+        Returns Square.X or Square.O if the corresponding player won the board.
+        returns None otherwise.
+        """
         state = self.state
         if state == State.X_WON:
             return Square.X
