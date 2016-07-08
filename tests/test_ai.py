8e43bae79337ce7cd5f23c9f2c7c62fc7ba5883b
stoimenoff <stoimenoffjr@gmail.com>
2016-07-08 14:27:06 +0300

    Heuristics tests added

--- tests/test_ai.py
+++ tests/test_ai.py
@@ -2,10 +2,19 @@ import unittest
 from unittest.mock import patch, PropertyMock
 
 from ultimatetictactoe.game.players.ai import *
-from ultimatetictactoe.game.boards import Macroboard, GameEndedError
+from ultimatetictactoe.game.boards import Macroboard, GameEndedError, Square
 
 
 class TestBots(unittest.TestCase):
+    MOVES = [(2, 2), (7, 6), (3, 2), (1, 7), (3, 4), (2, 5),
+             (7, 7), (4, 4), (4, 3), (4, 2), (3, 7), (0, 4), (2, 3),
+             (8, 2), (6, 8), (1, 8), (5, 7), (6, 5), (2, 8), (8, 6),
+             (6, 1), (0, 3), (0, 0), (2, 1), (8, 4), (6, 4), (1, 5),
+             (4, 7), (3, 3), (2, 0), (8, 1), (6, 3), (0, 2), (2, 7),
+             (8, 7), (6, 0), (1, 0), (5, 0), (6, 2), (0, 7), (2, 4),
+             (8, 8), (6, 7), (1, 3), (5, 2), (8, 0), (7, 0), (3, 0),
+             (1, 1), (5, 3), (7, 2), (3, 6), (5, 5), (7, 1), (3, 5)]
+
     def test_selector(self):
         self.assertIsInstance(select_bot(1), GentlemanBot)
         self.assertIsInstance(select_bot(2), RandomBot)
@@ -31,3 +40,31 @@ class TestBots(unittest.TestCase):
                 bot = select_bot(difficulty)
                 with self.assertRaises(GameEndedError):
                     bot.choose_move(board)
+
+    def test_score_macroboard(self):
+        board = Macroboard()
+        scores_x = [2, 0, 0, 0, 0, 3, 3, 5, 5, 8, 8, 8, 8, 8, 8, 10, 10, 12,
+                    12, 12, 10, 10, 10, 12, 12, 12, 12, 12, 10, 17, 17, 19, 19,
+                    23, 25, 25, 25, 25, 25, 25, 25, 25, 25, 29, 29, 29, 29, 29,
+                    29, 35, 33, 35, 37, 38, 34]
+        scores_o = [0, 0, 0, 0, 2, 2, 2, 2, 7, 7, 7, 7, 7, 7, 7, 7, 9, 9, 9, 9,
+                    11, 11, 15, 15, 15, 15, 17, 17, 19, 19, 19, 19, 22, 22, 24,
+                    26, 26, 26, 26, 26, 28, 30, 28, 26, 26, 28, 28, 26, 28, 28,
+                    33, 33, 35, 37, 45]
+        for i in range(len(self.MOVES)):
+            score_x = heuristics.score_macroboard(board, Square.X)
+            score_o = heuristics.score_macroboard(board, Square.O)
+            self.assertEqual(score_x, scores_x[i])
+            self.assertEqual(score_o, scores_o[i])
+            board.make_move(*self.MOVES[i])
+        score_x = heuristics.score_macroboard(board, Square.X)
+        score_o = heuristics.score_macroboard(board, Square.O)
+        self.assertEqual(score_x, 10000)
+        self.assertEqual(score_o, 33)
+
+        board = Macroboard()
+        for i in range(len(self.MOVES)):
+            score = heuristics.score(board)
+            expected = (scores_x[i] - scores_o[i]) * (-1)**i
+            self.assertEqual(score, expected)
+            board.make_move(*self.MOVES[i])
--- ultimatetictactoe/game/players/ai/__init__.py
+++ ultimatetictactoe/game/players/ai/__init__.py
@@ -3,5 +3,6 @@ from .heuristicsbot import HeuristicsBot
 from .randombot import RandomBot
 from .gentlemanbot import GentlemanBot
 from .botselector import select_bot
+from . import heuristics
 __all__ = ['HeuristicsBot', 'RandomBot', 'select_bot', 'GentlemanBot',
-           'AlphaBetaBot']
+           'AlphaBetaBot', 'heuristics']
